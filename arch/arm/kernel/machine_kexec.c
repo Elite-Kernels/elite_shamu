cf004bce794a44a609f0d4b253987ea11e0492ac
imoseyon <imoseyon@gmail.com>
2015-03-29 20:33:12 -0700

    arm: implement kexec-hardboot (by vbocek@gmail.com)
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to shamu, it is based of my hammerhead, mako, flo
      and grouper ports, which are based of Asus TF201 patch ported
      by Jens Andersen <jens.andersen@gmail.com>
    
      Shamu is rather similar to hammerhead - it uses device tree binary instead
      of atags. It is also rather picky - its bootloader erases first 128MB of RAM
      on reboot, so following hacks had to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.
    
      The guest kernel does not require any modifications, kexec-hardboot
      is equivalent to standard kexec in this aspect.
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Conflicts:
    	arch/arm/kernel/machine_kexec.c
    	arch/arm/kernel/relocate_kernel.S
    
    Conflicts:
    	boot.img
    
    Conflicts:
    	arch/arm/configs/shamu_defconfig

--- arch/arm/kernel/machine_kexec.c
+++ arch/arm/kernel/machine_kexec.c
@@ -14,16 +14,26 @@
 #include <asm/pgalloc.h>
 #include <asm/mmu_context.h>
 #include <asm/cacheflush.h>
+#include <asm/fncpy.h>
 #include <asm/mach-types.h>
 #include <asm/system_misc.h>
+#include <linux/memblock.h>
+#include <linux/of_fdt.h>
+#include <asm/mmu_writeable.h>
 
-extern const unsigned char relocate_new_kernel[];
+extern void relocate_new_kernel(void);
 extern const unsigned int relocate_new_kernel_size;
 
 extern unsigned long kexec_start_address;
 extern unsigned long kexec_indirection_page;
 extern unsigned long kexec_mach_type;
 extern unsigned long kexec_boot_atags;
+#ifdef CONFIG_KEXEC_HARDBOOT
+extern unsigned long kexec_hardboot;
+extern unsigned long kexec_boot_atags_len;
+extern unsigned long kexec_kernel_len;
+void (*kexec_hardboot_hook)(void);
+#endif
 
 static atomic_t waiting_for_crash_ipi;
 
@@ -49,12 +59,22 @@ int machine_kexec_prepare(struct kimage *image)
 					       current_segment->memsz))
 			return -EINVAL;
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+		if(current_segment->mem == image->start)
+			mem_text_write_kernel_word(&kexec_kernel_len, current_segment->memsz);
+#endif
+
 		err = get_user(header, (__be32*)current_segment->buf);
 		if (err)
 			return err;
 
 		if (be32_to_cpu(header) == OF_DT_HEADER)
-			kexec_boot_atags = current_segment->mem;
+		{
+			mem_text_write_kernel_word(&kexec_boot_atags, current_segment->mem);
+#ifdef CONFIG_KEXEC_HARDBOOT
+			mem_text_write_kernel_word(&kexec_boot_atags_len, current_segment->memsz);
+#endif
+		}
 	}
 	return 0;
 }
@@ -133,6 +153,8 @@ void machine_kexec(struct kimage *image)
 {
 	unsigned long page_list;
 	unsigned long reboot_code_buffer_phys;
+	unsigned long reboot_entry = (unsigned long)relocate_new_kernel;
+	unsigned long reboot_entry_phys;
 	void *reboot_code_buffer;
 
 	if (num_online_cpus() > 1) {
@@ -148,26 +170,34 @@ void machine_kexec(struct kimage *image)
 	reboot_code_buffer = page_address(image->control_code_page);
 
 	/* Prepare parameters for reboot_code_buffer*/
-	kexec_start_address = image->start;
-	kexec_indirection_page = page_list;
-	kexec_mach_type = machine_arch_type;
+	mem_text_write_kernel_word(&kexec_start_address, image->start);
+	mem_text_write_kernel_word(&kexec_indirection_page, page_list);
+	mem_text_write_kernel_word(&kexec_mach_type, machine_arch_type);
 	if (!kexec_boot_atags)
-		kexec_boot_atags = image->start - KEXEC_ARM_ZIMAGE_OFFSET + KEXEC_ARM_ATAGS_OFFSET;
-
+		mem_text_write_kernel_word(&kexec_boot_atags, image->start - KEXEC_ARM_ZIMAGE_OFFSET + KEXEC_ARM_ATAGS_OFFSET);
+#ifdef CONFIG_KEXEC_HARDBOOT
+	mem_text_write_kernel_word(&kexec_hardboot, image->hardboot);
+#endif
 
 	/* copy our kernel relocation code to the control code page */
-	memcpy(reboot_code_buffer,
-	       relocate_new_kernel, relocate_new_kernel_size);
+	reboot_entry = fncpy(reboot_code_buffer,
+			     reboot_entry,
+			     relocate_new_kernel_size);
+	reboot_entry_phys = (unsigned long)reboot_entry +
+		(reboot_code_buffer_phys - (unsigned long)reboot_code_buffer);
 
-
-	flush_icache_range((unsigned long) reboot_code_buffer,
-			   (unsigned long) reboot_code_buffer + KEXEC_CONTROL_PAGE_SIZE);
 	printk(KERN_INFO "Bye!\n");
 
 	if (kexec_reinit)
 		kexec_reinit();
 
-	soft_restart(reboot_code_buffer_phys);
+#ifdef CONFIG_KEXEC_HARDBOOT
+	/* Run any final machine-specific shutdown code. */
+	if (image->hardboot && kexec_hardboot_hook)
+		kexec_hardboot_hook();
+#endif
+
+	soft_restart(reboot_entry_phys);
 }
 
 void arch_crash_save_vmcoreinfo(void)
