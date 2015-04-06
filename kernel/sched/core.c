00c24ead02133635c897513b6609577988bf3a95
Brandon Berhent <bbedward@gmail.com>
2015-04-06 13:09:31 -0400

    sched/core: implement wake_up_if_idle

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1588,6 +1588,21 @@ static void ttwu_queue_remote(struct task_struct *p, int cpu)
 		smp_send_reschedule(cpu);
 }
 
+void wake_up_if_idle(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+	unsigned long flags;
+
+	if (!is_idle_task(rq->curr))
+		return;
+
+	raw_spin_lock_irqsave(&rq->lock, flags);
+	if (likely(is_idle_task(rq->curr)))
+		smp_send_reschedule(cpu);
+	/* Else cpu is not in idle, do nothing here */
+	raw_spin_unlock_irqrestore(&rq->lock, flags);
+}
+
 bool cpus_share_cache(int this_cpu, int that_cpu)
 {
 	return per_cpu(sd_llc_id, this_cpu) == per_cpu(sd_llc_id, that_cpu);
