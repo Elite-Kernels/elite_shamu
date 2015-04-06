79f3f6f61d00e684c07ea8fb7a45d3cdefd04692
Brandon Berhent <bbedward@gmail.com>
2015-04-06 13:11:04 -0400

    sched/core: add RCU protection to wake_up_if_idle

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1593,14 +1593,19 @@ void wake_up_if_idle(int cpu)
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long flags;
 
-	if (!is_idle_task(rq->curr))
-		return;
+	rcu_read_lock();
+
+	if (!is_idle_task(rcu_dereference(rq->curr)))
+		goto out;
 
 	raw_spin_lock_irqsave(&rq->lock, flags);
 	if (likely(is_idle_task(rq->curr)))
 		smp_send_reschedule(cpu);
 	/* Else cpu is not in idle, do nothing here */
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
+
+out:
+	rcu_read_unlock();
 }
 
 bool cpus_share_cache(int this_cpu, int that_cpu)
