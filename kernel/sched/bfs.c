09ba84e83950b96dab1a69bf4de7140e7b3ca3fa
Brandon Berhent <bbedward@gmail.com>
2015-04-09 23:22:06 -0400

    bfs: sync up some tick accounting from upstream

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -2191,15 +2191,11 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 #ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
 	if (static_key_false((&paravirt_steal_rq_enabled))) {
 		s64 steal = paravirt_steal_clock(cpu_of(rq));
-		u64 st;
 
 		steal -= rq->prev_steal_time_rq;
 
 		if (unlikely(steal > delta))
 			steal = delta;
-
-		st = steal_ticks(steal);
-		steal = st * TICK_NSEC;
 
 		rq->prev_steal_time_rq += steal;
 
@@ -2241,16 +2237,22 @@ static __always_inline bool steal_account_process_tick(void)
 {
 #ifdef CONFIG_PARAVIRT
 	if (static_key_false(&paravirt_steal_enabled)) {
-		u64 steal, st = 0;
+		u64 steal;
+		cputime_t steal_ct;
 
 		steal = paravirt_steal_clock(smp_processor_id());
 		steal -= this_rq()->prev_steal_time;
 
-		st = steal_ticks(steal);
-		this_rq()->prev_steal_time += st * TICK_NSEC;
+		/*
+		 * cputime_t may be less precise than nsecs (eg: if it's
+		 * based on jiffies). Lets cast the result to cputime
+		 * granularity and account the rest on the next rounds.
+		 */
+		steal_ct = nsecs_to_cputime(steal);
+		this_rq()->prev_steal_time += cputime_to_nsecs(steal_ct);
 
-		account_steal_time(st);
-		return st;
+		account_steal_time(steal_ct);
+		return steal_ct;
 	}
 #endif
 	return false;
