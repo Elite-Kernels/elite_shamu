82e4c2e254b6de5549025b47f6d78a7442db7c6f
buckmarble <forrestmarble@gmail.com>
2015-07-29 17:46:50 -0400

    Revert "BFS: [Sync] sched_setscheduler() logic, v2"
    This reverts commit 8ba9d9f6e33079b26bf09ed22589053fa584dfe0.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -566,17 +566,12 @@ static inline bool task_queued(struct task_struct *p)
 /*
  * Removing from the global runqueue. Enter with grq locked.
  */
-static inline void __dequeue_task(struct task_struct *p, int prio)
-{
-	list_del_init(&p->run_list);
-	if (list_empty(grq.queue + prio))
-		__clear_bit(prio, grq.prio_bitmap);
-	sched_info_dequeued(p);
-}
-
 static void dequeue_task(struct task_struct *p)
 {
-	__dequeue_task(p, p->prio);
+	list_del_init(&p->run_list);
+	if (list_empty(grq.queue + p->prio))
+		__clear_bit(p->prio, grq.prio_bitmap);
+	sched_info_dequeued(p);
 }
 
 /*
@@ -970,15 +965,11 @@ static inline void check_task_changed(struct rq *rq, struct task_struct *p,
 	 * our priority decreased, or if we are not currently running on
 	 * this runqueue and our priority is higher than the current's
 	 */
-	if (task_running(p)) {
+	if (rq->curr == p) {
 		reset_rq_task(rq, p);
 		/* Resched only if we might now be preempted */
 		if (p->prio > oldprio)
 			resched_curr(rq);
-	} else if (task_queued(p)) {
-		__dequeue_task(p, oldprio);
-		enqueue_task(p, rq);
-		try_preempt(p, rq);
 	}
 }
 
@@ -3944,7 +3935,7 @@ EXPORT_SYMBOL(sleep_on_timeout);
 void rt_mutex_setprio(struct task_struct *p, int prio)
 {
 	unsigned long flags;
-	int oldprio;
+	int queued, oldprio;
 	struct rq *rq;
 
 	BUG_ON(prio < 0 || prio > MAX_PRIO);
@@ -3971,10 +3962,17 @@ void rt_mutex_setprio(struct task_struct *p, int prio)
 
 	trace_sched_pi_setprio(p, prio);
 	oldprio = p->prio;
+	queued = task_queued(p);
+	if (queued)
+		dequeue_task(p);
 	p->prio = prio;
 	update_task_priodl(p);
-
-	check_task_changed(rq, p, oldprio);
+	if (task_running(p) && prio > oldprio)
+		resched_task(p);
+	if (queued) {
+		enqueue_task(p);
+		try_preempt(p, rq);
+	}
 
 out_unlock:
 	task_grq_unlock(&flags);
