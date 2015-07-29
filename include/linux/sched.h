2c412a950901fadb003d6643826ed0009f920f84
buckmarble <forrestmarble@gmail.com>
2015-07-29 17:49:10 -0400

    Revert "bfs: implement wake_up_if_idle"
    This reverts commit 2373c9012876a3ddbca2853fa03242f3a74d10a7.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -890,8 +890,6 @@ void free_sched_domains(cpumask_var_t doms[], unsigned int ndoms);
 
 bool cpus_share_cache(int this_cpu, int that_cpu);
 
-extern void wake_up_if_idle(int cpu);
-
 #else /* CONFIG_SMP */
 
 struct sched_domain_attr;
