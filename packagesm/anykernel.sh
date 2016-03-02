8a80d805d40cf999af39c16bd3a39493f70257b0
Forrest Marble <forrestmarble@gmail.com>
2016-03-01 22:58:16 -0500

    add packages and build scripts

---
+++ packagesm/anykernel.sh
@@ -0,0 +1,234 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=-Elite
+do.devicecheck=1
+do.initd=1
+do.system=1
+do.cleanup=1
+device.name1=shamu
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+
+OUTFD=/proc/self/fd/$1;
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
+  fi;
+  mv -f $ramdisk /tmp/anykernel/rdtmp;
+  mkdir -p $ramdisk;
+  cd $ramdisk;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+  if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
+    ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage-dtb ]; then
+    kernel=/tmp/anykernel/zImage-dtb;
+  else
+    kernel=`ls *-zImage-dtb`;
+    kernel=$split_img/$kernel;
+  fi;
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff  --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting..."; exit 1;
+  elif [ `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "New image larger than boot partition. Aborting..."; exit 1;
+  fi;
+  if [ -f "/data/custom_boot_image_patch.sh" ]; then
+    ash /data/custom_boot_image_patch.sh /tmp/anykernel/boot-new.img;
+    if [ $? != 0 ]; then
+      ui_print " "; ui_print "User script execution failed. Aborting..."; exit 1;
+    fi;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# replace_section <file> <begin search string> <end search string> <replacement string>
+replace_section() {
+  line=`grep -n "$2" $1 | cut -d: -f1`;
+  sed -i "/${2}/,/${3}/d" $1;
+  sed -i "${line}s;^;${4}\n;" $1;
+}
+
+# remove_section <file> <begin search string> <end search string>
+remove_section() {
+  sed -i "/${2}/,/${3}/d" $1;
+}
+
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5}\n;" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+insert_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;\n;" $1;
+    sed -i "$((line - 1))r $patch/$5" $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -pf $patch/$3 $1;
+  chmod $2 $1;
+}
+
+# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <original string> <replacement string>
+patch_fstab() {
+  entry=$(grep "$2" $1 | grep "$3");
+  if [ -z "$(echo "$entry" | grep "$6")" ]; then
+    case $4 in
+      block) part=$(echo "$entry" | awk '{ print $1 }');;
+      mount) part=$(echo "$entry" | awk '{ print $2 }');;
+      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
+      options) part=$(echo "$entry" | awk '{ print $4 }');;
+      flags) part=$(echo "$entry" | awk '{ print $5 }');;
+    esac;
+    newentry=$(echo "$entry" | sed "s;${part};${6};");
+    sed -i "s;${entry};${newentry};" $1;
+  fi;
+}
+
+## end methods
+
+
+## AnyKernel permissions
+# set permissions for included files
+chmod -R 755 $ramdisk
+chmod 640 $ramdisk/fstab.shamu
+#chmod 750 $ramdisk/init.rc
+#chmod 750 $ramdisk/init.shamu.rc
+chmod 750 $ramdisk/init.shamu.power.rc
+
+# backup then replace fstab and shamu.power.rc
+# backup_file fstab.shamu;
+# backup_file init.shamu.rc;
+# backup_file init.shamu.power.rc;
+replace_file fstab.shamu $ramdisk/fstab.shamu;
+#replace_file init.rc $ramdisk/init.rc;
+#replace_file init.shamu.rc $ramdisk/init.shamu.rc;
+replace_file init.shamu.power.rc $ramdisk/init.shamu.power.rc;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# default.prop
+# backup_file default.prop;
+replace_line default.prop "ro.adb.secure=0" "ro.adb.secure=1";
+replace_line default.prop "ro.adb.secure=1" "ro.adb.secure=1";
+replace_line default.prop "ro.secure=0" "ro.secure=1";
+replace_line default.prop "ro.secure=1" "ro.secure=1";
+append_file default.prop ro.adb.secure=1 default
+
+# init.rc
+# backup_file init.rc;
+append_file init.rc "/system/elite" init;
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
